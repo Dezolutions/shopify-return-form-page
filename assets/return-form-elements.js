bf003f1ce530859be67c282c80cad0fab345b0b4
Dezolutions <mars92851@gmail.com>
2023-09-25 21:34:21 +0300

    bugs fixed

--- assets/return-form-elements.js
+++ assets/return-form-elements.js
@@ -9,29 +9,8 @@ if (!customElements.get('order-select')) {
         this.orderSelect = this.querySelector('#orderSelect');
         this.productsTitle = this.querySelector('.products__title');
         this.productsList = this.querySelector('.products__list');
+        this.productListValues = this.querySelector('.product-list-values');
         this.orderSelect.addEventListener('change', this.onChangeOrder.bind(this));
-      }
-
-      createProductItemHTML(data) {
-
-        this.productsTitle.innerText = data.title;
-
-        //Remove old data
-        this.productsList.innerHTML = '';
-
-        data.items.map((item, index) => {
-          const li = document.createElement('li');
-          li.innerHTML = `
-            <label class="product-item">
-              <input class="product-item__checkbox" type="checkbox" value="${item.title}" name="contact[product_${index + 1}]"/>
-              <span class="product-item__custom-checkbox"></span>
-              <div class="product-item__img-block"><img src="${item.img}"/></div>
-              <p>${item.title}</p>
-            </label>
-          `;
-
-          this.productsList.appendChild(li);
-        })
       }
 
       onChangeOrder(event) {
@@ -58,6 +37,20 @@ if (!customElements.get('order-select')) {
                 `;
                 this.productsList.appendChild(li);
               })
+
+              const productItemCheckboxes = document.querySelectorAll('.product-item__checkbox');
+              const productsListInput = document.querySelector('.products-list-values');
+
+              productItemCheckboxes.forEach(checkbox => {
+                checkbox.addEventListener('change', event => {
+                
+                  if (checkbox.checked) {
+                    productsListInput.value += checkbox.value + ', ';
+                  } else {
+                    productsListInput.value = productsListInput.value.replace(checkbox.value + ', ', '');
+                  }
+                });
+              });
             })
         } catch (error) {
           console.log(error)
@@ -81,60 +74,59 @@ if (!customElements.get('return-form')) {
         this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
         this.successMessage = this.querySelector('.return-form__success');
         this.loader = this.querySelector('.loader');
-        this.productItemCheckboxes = this.querySelectorAll('.product-item__checkbox');
+        this.productListValues = this.querySelector('.products-list-values');
+        this.errorMessage = this.querySelector('.return-form__error');
       }
 
       onSubmitHandler(e) {
         e.preventDefault();
+        this.successMessage.classList.add('none');
+        const formData = new FormData(this.form);
+        const formValues = {};
 
-        console.log(this.productItemCheckboxes)
-        let atLeastOneCheckboxChecked = false;
-
-        for (let i = 0; i < this.productItemCheckboxes.length; i++) {
-          if (this.productItemCheckboxes[i].checked) {
-            atLeastOneCheckboxChecked = true;
-            break;
-          }
+        for (const entry of formData.entries()) {
+          formValues[entry[0]] = entry[1];
         }
 
-        
-          const formData = new FormData(this.form);
-          const formValues = {};
-
-          for (const entry of formData.entries()) {
-            formValues[entry[0]] = entry[1];
-          }
+        if(this.productListValues != null && this.productListValues.value == '') {
+          this.errorMessage.classList.remove('none')
+          this.errorMessage.innerText = 'Choose atleast one product!'
+        } else {
+          this.errorMessage.classList.add('none')
+          this.errorMessage.innerText = '';
           this.loader.classList.remove('loader-none')
-          this.form.querySelector('.return-form__fields').classList.add('submitting');
-          // Send the form data to the API Gateway.
-          fetch('https://bfg7yldc83.execute-api.us-east-1.amazonaws.com/new/submitdata', {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify(formValues)
-          })
-            .then(response => response.json())
-            .then(response => {
-              console.log(response)
-              this.form.querySelectorAll('input, select, textarea').forEach((field) => {
-                field.value = '';
-              });
-              this.form.querySelector('.return-form__fields').classList.remove('submitting');
-              this.loader.classList.add('loader-none');
-              this.successMessage.innerText = response.body;
-
-              if(this.productsList && this.productsTitle) {
-                this.productsTitle.innerText = 'Products:';
-                this.productsList.innerHTML = 'Select order to choose products for return!';
-              }
-              
-            })
-
-            .catch(error => {
-              console.log(error);
+        this.form.querySelector('.return-form__fields').classList.add('submitting');
+        // Send the form data to the API Gateway.
+        fetch('https://bfg7yldc83.execute-api.us-east-1.amazonaws.com/new/submitdata', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify(formValues)
+        })
+          .then(response => response.json())
+          .then(response => {
+            this.form.querySelectorAll('input, select, textarea').forEach((field) => {
+              field.value = '';
             });
+            this.form.querySelector('.return-form__fields').classList.remove('submitting');
+            this.loader.classList.add('loader-none');
+            this.successMessage.classList.remove('none');
+            this.successMessage.innerText = response.body;
+
+            if (this.productsList && this.productsTitle) {
+              this.productsTitle.innerText = 'Products:';
+              this.productsList.innerHTML = 'Select order to choose products for return!';
+            }
+
+          })
+
+          .catch(error => {
+            console.log(error);
+          });
+        }
         
+
 
       }
     }
